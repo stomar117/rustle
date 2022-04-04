268dffcbbb71c0593ba21702740fab57eea20293
theEndurance-del <tomarshubhanshu@protonmail.ch>
2022-04-04 16:25:53 +0530

    Use `match` instead of if-else ladder

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #[path="read_util/reader.rs"] mod reader;
+use std::ops::Deref;
+
 use crossterm::{execute,terminal::{Clear, ClearType},cursor::MoveTo};
 use reader::read_enum::ReadEnum;
 
@@ -10,19 +12,28 @@ fn main() {
         let read_value = token_reader.read();
         match read_value {
             ReadEnum::Command(cmd) => {
-                if cmd == "exit" {
-                    break;
+                match cmd.deref() {
+                    "exit" => {break;}
+                    "clear" => {
+                        execute!(std::io::stdout(),
+                            Clear(ClearType::All),
+                            MoveTo(0,0)).expect("failed to clear screen");
+                    }
+                    _ => {println!("{}", cmd);}
                 }
-                else if cmd == "clear" {
-                    execute!(std::io::stdout(),
-                        Clear(ClearType::All),
-                        MoveTo(0,0)
-                    )
-                        .expect("Failed to clear screen");
-                }
-                else {
-                    println!("{}", cmd);
-                }
+                // if cmd == "exit" {
+                //     break;
+                // }
+                // else if cmd == "clear" {
+                //     execute!(std::io::stdout(),
+                //         Clear(ClearType::All),
+                //         MoveTo(0,0)
+                //     )
+                //         .expect("Failed to clear screen");
+                // }
+                // else {
+                //     println!("{}", cmd);
+                // }
             },
             ReadEnum::KeyboardInterrupt => {
                 println!("^C");
